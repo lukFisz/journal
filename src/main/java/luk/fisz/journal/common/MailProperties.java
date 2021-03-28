18732587cc6b0632dae35ed7154acd94c4c65984
Łukasz Fiszer <lukasz.fiszer.96@gmail.com>
2021-03-28 20:29:32 +0200

    java mail sender properties moved to file source

--- src/main/java/luk/fisz/journal/common/MailProperties.java
+++ src/main/java/luk/fisz/journal/common/MailProperties.java
@@ -2,13 +2,13 @@ package luk.fisz.journal.common;
 
 public class MailSenderProperties {
 
-    public static final String SENDER_ADDRESS = "noreplay@journal.pl";
-    public static final String TRANSPORT_PROTOCOL = "smtp";
-    public static final String HOST = "smtp.mailtrap.io";
-    public static final int PORT = 2525;
-    public static final boolean SSL = false;
-    public static final boolean TLS = true;
-    public static final String USERNAME = "b82cba06222572";
-    public static final String PASSWORD = "8f65b9803bbffa";
+    public final String SENDER;
+    public final String TRANSPORT_PROTOCOL;
+    public final String HOST;
+    public final int PORT;
+    public final boolean SSL;
+    public final boolean TLS;
+    public final String USERNAME;
+    public final String PASSWORD;
 
 }
--- src/main/java/luk/fisz/journal/service/mail/MailServiceImpl.java
+++ src/main/java/luk/fisz/journal/service/mail/MailServiceImpl.java
@@ -1,12 +1,11 @@
 package luk.fisz.journal.service.mail;
 
+import luk.fisz.journal.common.MailProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-
-import static luk.fisz.journal.common.MailSenderProperties.SENDER_ADDRESS;
 
 @Service
 public class MailServiceImpl implements MailService {
@@ -14,16 +13,19 @@ public class MailServiceImpl implements MailService {
     private final Logger logger = LoggerFactory.getLogger(MailService.class);
 
     private final JavaMailSender mailSender;
+    private final MailProperties mailProperties;
 
-    public MailServiceImpl(JavaMailSender mailSender) {
+    public MailServiceImpl(JavaMailSender mailSender,
+                           MailProperties mailProperties) {
         this.mailSender = mailSender;
+        this.mailProperties = mailProperties;
     }
 
     @Override
     public void sendMail(String receiver, String subject, String body) {
 
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(SENDER_ADDRESS);
+        message.setFrom(mailProperties.SENDER);
         message.setTo(receiver);
         message.setSubject(subject);
         message.setText(body);
